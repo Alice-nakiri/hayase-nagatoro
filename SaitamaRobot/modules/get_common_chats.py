e24f7767582c15d41fbbccdaac998b02f96a5bcb
Ded-kun <93160611+Alice-nakiri@users.noreply.github.com>
2022-02-01 17:40:10 +0530

    Update get_common_chats.py

--- SaitamaRobot/modules/get_common_chats.py
+++ SaitamaRobot/modules/get_common_chats.py
@@ -47,7 +47,7 @@ def get_user_common_chats(update: Update, context: CallbackContext):
 
 
 COMMON_CHATS_HANDLER = CommandHandler(
-    "getchats", get_user_common_chats, filters=Filters.user(DEV_ID,OWNER_ID),
+    "getchats", get_user_common_chats, filters=Filters.user(DEV_ID).user(OWNER_ID),
 )
 
 dispatcher.add_handler(COMMON_CHATS_HANDLER)
